a312647a5f2c87c5cf20cf756d68c328a53e168b
Jyeong2c <tho1339@naver.com>
2022-12-19 21:23:24 +0900

    22.12.19

---
+++ 22.11.24/input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+
+char*input()
+{
+	char*str;
+	str =(char*)malloc(BUFSIZ);
+	scanf("%s",str);
+
+	return str;
+}
+
--- limain.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-int main()
-{
-		printf("Hellow, World!\n");
-		exit(0);
-}
